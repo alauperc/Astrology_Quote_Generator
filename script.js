e6c21c50d8b7a8aa3c39a6ac709b047df88937e9
Angel Lau <66029625+alauperc@users.noreply.github.com>
2020-11-10 16:24:01 -0500

    Start  and  function

---
+++ script.js
@@ -0,0 +1,24 @@
+const randomGenerator = (month, day, year) => {
+
+}
+
+// Helper methods
+
+const zodiacFinder = (dateOfBirth) => {
+  let zodiacSigns = { 
+    Aries: [],
+    Taurus: [],
+    Gemini: [],
+    Cancer: [],
+    Leo: [],
+    Virgo: [],
+    Libra: [],
+    Scorpio: [],
+    Sagittarius: [],
+    Capricorn: [],
+    Aquarius: [],
+    Pisces: []
+  }
+
+
+}
